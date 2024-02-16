ca43c24fa5367bcad1cbee2bedb70b495119aab3
LucasBXavier <lucas_b.xavier@hotmail.com>
2024-02-16 00:15:07 -0300

    intrudução a EF

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,49 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+using SQLitePCL;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
+{
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _context.Eventos;
+
+    }
+
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+        return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+
+    }
+
+    [HttpPost]
+    public string Post()
+    {
+        return "Exemplo Post";
+    }
+    [HttpPut("{id}")]
+    public string Put(int id)
+    {
+        return $"Exemplo Put com Id = {id}";
+    }
+    [HttpDelete("{id}")]
+    public string Delete(int id)
+    {
+        return $"Exemplo Delete com Id = {id}";
+    }
+}
